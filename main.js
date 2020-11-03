682c510cb2b34cd1551d1cef18ec68d8328d3ea3
ryusugimo <72963784+ryusugimo@users.noreply.github.com>
2020-11-03 19:04:01 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = promppt('Please enter your name.');
+  let myName = prompt('Please enter your name.');
   if(!myName || myName === null) {
     setUserName();
   } else {
